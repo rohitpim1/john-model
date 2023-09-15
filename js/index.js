b203000c3154fcf53bfb2bbb3eb6ca48633f3d88
Rohit pimpalkar <pimrohit33@gmail.com>
2023-09-15 16:40:44 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,17 +5,17 @@ function showImg(picNum) {
   mod[0].style.top = document.documentElement.scrollTop - 150 + "px";
 
   if (Number(picNum) == 1) {
-    mod[0].style.backgroundImage = "url('/assests/pic1.png')";
+    mod[0].style.backgroundImage = "url('../assests/pic1.png')";
   } else if (Number(picNum) == 2) {
-    mod[0].style.backgroundImage = "url('/assests/pic2.png')";
+    mod[0].style.backgroundImage = "url('../assests/pic2.png')";
   } else if (Number(picNum) == 3) {
-    mod[0].style.backgroundImage = "url('/assests/pic3.png')";
+    mod[0].style.backgroundImage = "url('../assests/pic3.png')";
   } else if (Number(picNum) == 4) {
-    mod[0].style.backgroundImage = "url('/assests/pic4.png')";
+    mod[0].style.backgroundImage = "url('../assests/pic4.png')";
   } else if (Number(picNum) == 5) {
-    mod[0].style.backgroundImage = "url('/assests/pic5.png')";
+    mod[0].style.backgroundImage = "url('../assests/pic5.png')";
   } else {
-    mod[0].style.backgroundImage = "url('/assests/pic6.png')";
+    mod[0].style.backgroundImage = "url('../assests/pic6.png')";
   }
 }
 
