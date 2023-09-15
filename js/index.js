38cfab839e3c2ab2b2d19bda0e521b2ad15f14d7
rohitpim1 <pimrohit33@gamil.com>
2023-09-15 15:39:47 +0530

    all done

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let mod = document.getElementsByClassName("mod");
+
+function showImg(picNum) {
+  mod[0].style.display = "block";
+  mod[0].style.top = document.documentElement.scrollTop - 150 + "px";
+
+  if (Number(picNum) == 1) {
+    mod[0].style.backgroundImage = "url('/assests/pic1.png')";
+  } else if (Number(picNum) == 2) {
+    mod[0].style.backgroundImage = "url('/assests/pic2.png')";
+  } else if (Number(picNum) == 3) {
+    mod[0].style.backgroundImage = "url('/assests/pic3.png')";
+  } else if (Number(picNum) == 4) {
+    mod[0].style.backgroundImage = "url('/assests/pic4.png')";
+  } else if (Number(picNum) == 5) {
+    mod[0].style.backgroundImage = "url('/assests/pic5.png')";
+  } else {
+    mod[0].style.backgroundImage = "url('/assests/pic6.png')";
+  }
+}
+
+function closeImg() {
+  mod[0].style.display = "none";
+}
